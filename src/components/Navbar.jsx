26c8d87b582500fde168cddbe236c2ae4c45e77d
Josh <joshuakibwage23@gmail.com>
2025-03-03 16:03:05 +0300

    Add mobile responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import Button from "../components/Button"
 import { NavLink } from "react-router-dom"
 import { Link } from "react-router-dom"
+import { LuMenu } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react"
+import ResponsiveMenu from "./ResponsiveMenu";
 
 
 
@@ -51,9 +55,17 @@ const navLinks = [
 
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+
+  const toggleNav = () => {
+    setNav(!nav)
+  }
+
+
   return (
-    <header>
-      <nav className="container mx-auto py-4 flex justify-between items-center">
+    <header className="border-b-2 border-primary fixed top-0 left-0 right-0 z-10">
+      <nav className="container mx-auto py-4 justify-between items-center flex">
         {/* logo */}
         <Link to ='/' className="flex flex-col justify-center hover:translate-x-1 delay-200 ease-in-out transition-all">
           <h1 className="font-cursive text-4xl ">
@@ -62,7 +74,7 @@ const Navbar = () => {
           <p  className="text-xs tracking-[0.5rem] text-center text-primary">WORK OUT</p>
         </Link>
         {/* nav links */}
-        <ul className="flex gap-10 items-center">
+        <ul className="hidden md:flex gap-6 items-center">
           {
             navLinks.map(({id, path, name}) => (
               <li className="text-white uppercase text-sm hover:text-primary transition-all delay-200
@@ -86,9 +98,23 @@ const Navbar = () => {
           <Button 
             label="Log In"
             className="bg-primary hover:bg-primary/80 px-8 py-2 hover:scale-105 delay-200 transition-transform
-            ease-in-out cursor-pointer rounded-md"/>
+            ease-in-out cursor-pointer rounded-md hidden lg:block"/>
         </ul>
+        <button onClick={toggleNav} className="flex md:hidden cursor-pointer">
+          {
+            nav ?  (
+              <IoClose size={30} />
+            ) : (
+              <LuMenu size={30} />
+            )
+          }
+        </button>
       </nav>
+
+      {/* responsive navbar */}
+      <ResponsiveMenu 
+        nav={nav}
+      />
     </header>
   )
 }
---
+++ src/components/ResponsiveMenu.jsx
@@ -0,0 +1,104 @@
+import { NavLink } from "react-router-dom"
+import { FaUserCircle } from "react-icons/fa";
+import Button from "./Button";
+
+const navLinks = [
+    {
+      id:1,
+      path: '/',
+      name: "home"
+  
+    },
+    {
+      id: 2,
+      path: '/about',
+      name: "about"
+  
+    },
+    {
+      id: 3,
+      path: '/blog',
+      name: "blog"
+  
+    },
+    {
+      id: 4,
+      path: '/classes',
+      name: "classes"
+  
+    },
+    {
+      id: 5,
+      path: '/contact',
+      name: "contact"
+  
+    },
+    {
+      id: 6,
+      path: '/pricing',
+      name: "pricing"
+  
+    },
+    {
+      id: 7,
+      path: '/trainers',
+      name: "trainers"
+  
+    },
+  ];
+  
+
+const ResponsiveMenu = ({ nav }) => {
+  return (
+    <div
+      className={`${
+        nav ? "left-0" : "-left-[100%]"
+      } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-black border-r-2 border-primary/20
+       dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl 
+       shadow-md`}
+    >
+      <div className="card">
+        <div className="flex items-center justify-start gap-3">
+          <FaUserCircle size={50} />
+          <div>
+            <h1>Hello User</h1>
+            <h1 className="text-sm text-slate-500">Premium user</h1>
+          </div>
+        </div>
+        <ul className="hidden md:flex gap-6 items-center">
+          {
+            navLinks.map(({id, path, name}) => (
+              <li className="text-white uppercase text-sm hover:text-primary transition-all delay-200
+              ease-in-out">
+                <NavLink 
+                  key={id} 
+                  to={path}
+                  className={({isActive, isPending}) => (
+                    isActive
+                    ? "active"
+                    : isPending
+                    ? "pending"
+                    : ""
+                  )}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ) )
+          }
+          <Button 
+            label="Log In"
+            className="bg-primary hover:bg-primary/80 px-8 py-2 hover:scale-105 delay-200 transition-transform
+            ease-in-out cursor-pointer rounded-md hidden lg:block"/>
+        </ul>
+      </div>
+      <div className="footer">
+        <h1>
+          Made with ❤ by <a href="https://github.com/Joshuakibwage/">Joshua</a>{" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default ResponsiveMenu;
